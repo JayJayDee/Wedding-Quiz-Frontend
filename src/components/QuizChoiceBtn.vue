4d3b116688b572c4a035fba22ce41aa5c16e4166
jaydee <jindongp@gmail.com>
2018-08-12 15:48:07 +0900

    added QuizPlay sub-components. removed unsed component

---
+++ src/components/QuizChoiceBtn.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <v-btn>
+  </v-btn>  
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  props: {
+    choice_no: Number,
+    content: String
+  }
+})
+export default class QuizChoiceBtn extends Vue {
+  
+}
+
+</script>
---
+++ src/components/QuizQustionRow.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <v-btn>
+  </v-btn>  
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  props: {
+    type: String,
+    content: String
+  }
+})
+export default class QuizQuestionRow extends Vue {
+  
+}
+
+</script>
