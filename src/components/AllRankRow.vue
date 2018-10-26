ca02c94dbd016aa358cb278443dc2e39decc4448
jaydee <jindongp@gmail.com>
2018-10-26 23:09:33 +0900

    added phone expression

--- src/components/AllRankRow.vue
+++ src/components/AllRankRow.vue
@@ -8,6 +8,7 @@
       <v-list-tile-title>
         <span class="text--primary">{{ rankerName }}</span>
         {{ rankNumberExpr }}
+        <span class="rank-phone">&mdash; {{ rankPhoneExpr }}</span>
       </v-list-tile-title>
       <v-list-tile-sub-title>
         <span class="text--primary">{{ rankScoreExpr }}점 획득</span> 
@@ -50,6 +51,12 @@ export default class AllRankRow extends Vue {
     return `${this.rank.rank}등`;
   }
 
+  public get rankPhoneExpr(): string {
+    if (!this.rank) return '';
+    const lastNum: string = this.rank.phone.slice(-4);
+    return `XXX-XXXX-${lastNum}`;
+  }
+
   public get rankScoreExpr(): string {
     if (!this.rank) return '';
     return `${this.rank.score_sum}`;
@@ -73,5 +80,8 @@ export default class AllRankRow extends Vue {
 </script>
 
 <style scoped>
-
+.rank-phone {
+  font-size: 11pt;
+  color: #999999;
+}
 </style>
