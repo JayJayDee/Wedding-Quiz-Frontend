63969f50105675f6f21c0f711a093eb8466503f1
JayJayDee <jindongp@gmail.com>
2020-04-09 16:26:43 +0900

    defined component vue-router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from '../views/Home.vue';
+import Quiz from '../views/Quiz.vue';
+import Rank from '../views/Rank.vue';
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/quiz',
+    name: 'Quiz',
+    component: Quiz
+  },
+  {
+    path: '/rank',
+    name: 'Rank',
+    component: Rank
   }
 ];
 
